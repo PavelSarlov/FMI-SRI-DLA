6903b7175d2c476f7b385c7fbafcf9eb18738ccc
Pavel Sarlov <sarlovpavel@gmail.com>
2022-01-20 12:50:55 +0200

    finish hw3

--- homework/hw3/generator.py
+++ homework/hw3/generator.py
@@ -24,8 +24,33 @@ def generateCode(model, char2id, startSentence, limit=1000, temperature=1.):
     ###  Тук следва да се имплементира генерацията на текста
     #############################################################################
     #### Начало на Вашия код.
+    
+    model.eval()
 
-    pass
+    device = next(model.parameters()).device
+    X = torch.tensor([char2id[c] for c in result] if len(result) > 0 else [model.unkTokenIdx], dtype=torch.long, device=device)
+    E = model.embed(X[None, :])                                  
+
+    with torch.no_grad():
+        out, hc_n = model.lstm(E)
+
+        while len(result) < limit:
+            out = model.dropout(out[-1])
+            Z = model.projection(out.flatten(0, 1))
+            p = torch.nn.functional.softmax(Z, dim = 0)
+            prob = np.array(p).astype(np.float64)
+            prob /= prob.sum()
+
+            c = np.random.choice(list(char2id.keys()), p=prob)
+            if char2id[c] == model.endTokenIdx:
+                break
+
+            result += c
+
+            X = torch.tensor([char2id[c]], dtype=torch.long, device=device)
+            E = model.embed(X[None, :])
+
+            out, hc_n = model.lstm(E, hc_n)
     
     #### Край на Вашия код
     #############################################################################
