61c062d4043293fcda3f67e720804d9673b430f7
Pavel Sarlov <sarlovpavel@gmail.com>
2022-02-04 23:21:57 +0200

    update

--- project/parameters.py
+++ project/parameters.py
@@ -13,8 +13,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 enc_hid_size = 128
 dec_hid_size = 128
-enc_dropout = 0.2
-dec_dropout = 0.2
+enc_dropout = 0.1
+dec_dropout = 0.1
 enc_layers = 3
 dec_layers = 3
 enc_heads = 8
@@ -26,7 +26,7 @@ limit = 1000
 
 uniform_init = 0.1
 learning_rate = 0.0005
-clip_grad = 5.0
+clip_grad = 1.0
 learning_rate_decay = 0.5
 
 batchSize = 32
